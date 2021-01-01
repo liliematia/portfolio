3598221093d436b6a27b889d24bb8362ea86ada7
Lilie de la Motte <liliedelamotte@gmail.com>
2020-12-31 18:35:10 -0800

    change site title

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Lilie Matia'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'lilie.codes'
 
 export default function Layout({ children, home }) {
   return (
